cdb27952be7d48e31d3013e7503e9e543114de42
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2025-06-30 14:52:59 +0500

    cors error

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://future-tech-ai.netlify.app/","https://future-tech-ai.netlify.app/"],
+    origin: ["http://localhost:3000", "https://future-tech-ai.netlify.app"],
     credentials: true,
   })
 );
