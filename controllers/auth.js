44cfd13e26922e4ab20d07e9806e67ca0c08e3f1
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2025-07-23 16:47:15 +0500

    ew

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,7 +48,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // ✅ only true in production
-      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // ✅ avoids issues locally
+      sameSite: process.env.NODE_ENV === "production" ? "Lax" : "Lax", // ✅ avoids issues locally
       path: "/", // required for all routes
     });
 
