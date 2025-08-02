41e20c97b33f58b14b758a55b2a6843d82be422d
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2025-08-02 16:22:52 +0500

    Image Required Updated

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,9 +61,6 @@ export const register = async (req, res) => {
     return res.status(400).json({ message: "All fields are required" });
   }
 
-  if (!req.file) {
-    return res.status(400).json({ message: "Profile Image is required" });
-  }
 
   const file = await uploadImageToCloudinary(req.file.path);
 
